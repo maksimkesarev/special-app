6ef7c1dadc199f27fd3b227c481cb3560d24040a
maksimkesarev <151756836+maksimkesarev@users.noreply.github.com>
2023-11-22 18:35:19 +0300

    Update Main.java
    dd -> DD

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,7 @@ import java.time.format.DateTimeFormatter;
 public class Main {
 
     public static void main(String[] args) {
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm:ss");
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("DD.MM.yyyy HH:mm:ss");
         LocalDateTime now = LocalDateTime.now();
         System.out.println(now.format(formatter));
     }
